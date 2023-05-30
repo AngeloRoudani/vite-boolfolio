bc9c6128ee787e9894ac224bd8e82987c5904572
AngeloRoudani <ange.rouda@gmail.com>
2023-05-30 17:09:57 +0200

    esercizio base concluso

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,7 @@ export default {
                 .then(response => {
                 
                     this.projects = response.data.results;
+                    
                 });
         }
 
